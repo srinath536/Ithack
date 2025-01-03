685ca30bd8d6bc910896b1065003d8a3a1a01212
srinath536 <Your_Email>
2025-01-03 14:02:04 +0530

    media query for herosec

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -7,10 +7,7 @@ const HeroSection: React.FC = () => {
   return (
     <section className="hero-section">
       <div className="hero-overlay">
-        <h1>
-          <span className="hackathonix">HACKATHONIX</span>
-          <span className="gradient-25"> 25</span>
-        </h1>
+        <h1>HACKATHONIX 25</h1>
         <a href={googleFormURL} target="_blank" rel="noopener noreferrer">
           <button className="register-btn">Register Now</button>
         </a>
